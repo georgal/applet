4ace5de47d9eea7b954132315b4e95a5bd159d38
George <george.georgalidis@gmail.com>
2018-04-28 07:55:41 +0300

    Delete filter.js

--- assets/js/filter.js
+++
@@ -1,9 +0,0 @@
-                  function myFunction() {
-          var x = document.getElementById("enabler");
-          var y = document.getElementById("hider");
-          if (y.style.display === "none") {
-              y.style.display = "block";
-              x.style.display = "none";
-							event.preventDefault();
-			}
-			}
